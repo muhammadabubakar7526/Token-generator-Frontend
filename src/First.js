fac8190a93f5f833c81562bf3b4a50304fd938f2
abubakar528 <muhammadabubakar7526@gmail.com>
2022-08-22 21:26:10 +0500

    create token and import automatic in your metamask wallte

--- src/First.js
+++ src/First.js
@@ -9,6 +9,10 @@ import abi from './contractAbi.json'
 import byteCode from './contractByteCode'
 const First =  () => {
 const [connect, setConnect] = useState(false);
+const [name, setName] = useState('');
+const[address, setAddress] = useState();
+const [symbol, setSymbol] = useState();
+const [decimals, setDecimals] = useState();
   const formik = useFormik({
     // set initail values to the form fields
     initialValues: {
@@ -31,7 +35,6 @@ const [connect, setConnect] = useState(false);
       deployFunction(values);
     },
   })
- 
   // ..............................................
  const deployFunction = async (value) => {
 // get signer 
@@ -40,15 +43,15 @@ await provider.send("eth_requestAccounts", []);
 const signer = await provider.getSigner();
 const address = await signer.getAddress()
 console.log(address, "this is signer aqddress")
-
-
-
 const factory = new ContractFactory(abi, byteCode,signer);
 console.log(value.tokenName, "this is in deploy function");
-console.log(value.totalSupply, "this is total supply");
-
+console.log(value.tokenSymbol, "this is total supply");
+setSymbol(value.tokenSymbol);
+setName(value.tokenName);
+setDecimals(value.tokenDecimals);
 const contract = await factory.deploy(value.tokenName, value.tokenSymbol, value.tokenDecimals, value.initialSupply, value.totalSupply);
 console.log(contract.address);
+setAddress(contract.address);
 console.log(contract.deployTransaction);
  }
   // ...............................................
@@ -60,20 +63,54 @@ console.log(contract.deployTransaction);
     console.log(address)
     setConnect(true)
   }
+
+  // auto import token funciton in metamask wallet
+  const autoImportToken = async () => {
+    alert("auto import token")
+    const tokenAddress = address;
+    const tokenSymbol = symbol;
+    const tokenDecimals =decimals;
+    try {
+      // wasAdded is a boolean. Like any RPC method, an error may be thrown.
+      const wasAdded = await window.ethereum.request({
+        method: 'wallet_watchAsset',
+        params: { 
+          type: 'ERC20', // Initially only supports ERC20, but eventually more!
+          options: {
+            address: tokenAddress, // The address that the token is at.
+            symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
+            decimals: tokenDecimals, // The number of decimals in the token
+          },
+        },
+      });
+    
+      if (wasAdded) {
+        console.log('Thanks for your interest!');
+      } else {
+        console.log('Your loss!');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  // ...............................................
+
   return (
     <>
       <div className="container-fluid">
         <div className="container" id="main">
           <div className="container" id="upper">
-            <div className="col-lg-8 md-8 col-sm-8 col-8">
+            <div id="up1" className="col-lg-8 col-md-7 col-sm-8 ">
               {' '}
               <h4 style={{ fontSize: '24px', fontWeight: '500' }}>
                 Token Details
-              </h4>
+              </h4> 
               <p id="small">Enter token details and choose your network</p>
+              <h4>Token Name :{name}</h4>
+              <h4>Address:{address}</h4>
             </div>
-            <div id="connectBtn" className="col-lg-4 md-4 col-sm-4 col-4">
-              <button className="cntBtn" onClick={connectWallet}>
+            <div id="connectBtn" className="col-lg-4 col-md-4 col-sm-2 col-6">
+              <button  className="cntBtn" onClick={connectWallet}>
                 {connect ? 'Connected' : 'Connect Wallet'}
               </button>
             </div>
@@ -166,8 +203,8 @@ console.log(contract.deployTransaction);
                 <label id="label">initial supply*</label>
                 <div id="a2">
                   <input
-                    type="text"
-                    id="input2"
+                    type="number"
+                    id="input"
                     class="form-control"
                     name="initialSupply"
                     onChange={formik.handleChange}
@@ -189,7 +226,7 @@ console.log(contract.deployTransaction);
                 <label id="label">total supply *</label>
                 <div id="a2">
                   <input
-                    type="text"
+                    type="number"
                     id="input2"
                     class="form-control"
                     name="totalSupply"
@@ -456,9 +493,10 @@ console.log(contract.deployTransaction);
                     </p>
                   </div>
                 </div>
-                <button type="submit" onClick={formik.handleSubmit}id="submitBtn">
+                <button type="submit" onClick={formik.handleSubmit} id="submitBtn">
                   NEXT
                 </button>
+                <button onClick={autoImportToken} className="cntBtn2 col-lg-12 col-md-12 col-sm-12 col-12">Auto Import Token </button>
               </div>
             </div>
           </div>
--- src/Validate.js
+++ src/Validate.js
@@ -3,9 +3,9 @@ import * as Yup from 'yup';
   export const LoginSchema = Yup.object().shape({
     tokenName: Yup.string().required("name is required"),
     tokenSymbol: Yup.string().required("symbol must required"),
-    tokenDecimals: Yup.string().required("Enter 18"),
-    initialSupply: Yup.string().required("must provide initial supply"),
-    totalSupply: Yup.string().required("total supply required"),
+    tokenDecimals: Yup.string().required("decimals must be 18"),
+    initialSupply: Yup.number().required("must provide initial supply").moreThan(0, "initail supply must be greater than 0"),
+    totalSupply: Yup.number().required("total supply required").moreThan(0, "total supply must be greater than 0"),
     
   });
 
