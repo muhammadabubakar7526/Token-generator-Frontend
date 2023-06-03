f43186b7f0033e2c68b25b1b4d7a88f387077410
abubakar528 <muhammadabubakar7526@gmail.com>
2023-06-03 17:27:25 +0500

    github actions added

--- src/First.js
+++ src/First.js
@@ -1,118 +1,115 @@
-import React, { useState } from 'react'
-import Form from 'react-bootstrap/Form'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { useFormik } from 'formik'
-import { ethers } from 'ethers'
-import { LoginSchema } from './Validate'
-import { ContractFactory } from 'ethers'
-import abi from './contractAbi.json'
-import byteCode from './contractByteCode'
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useFormik } from "formik";
+import { ethers } from "ethers";
+import { LoginSchema } from "./Validate";
+import { ContractFactory } from "ethers";
+import abi from "./contractAbi.json";
+import byteCode from "./contractByteCode";
 const First = () => {
-  const [connect, setConnect] = useState(false)
-  const [name, setName] = useState('')
-  const [address, setAddress] = useState()
-  const [symbol, setSymbol] = useState()
-  const [decimals, setDecimals] = useState()
-  const [ value, setValue] = useState();
+  const [connect, setConnect] = useState(false);
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState();
+  const [symbol, setSymbol] = useState();
+  const [decimals, setDecimals] = useState();
+  const [value, setValue] = useState();
   const formik = useFormik({
     // set initail values to the form fields
     initialValues: {
-      tokenName: '',
-      tokenSymbol: '',
-      tokenDecimals: '',
-      initialSupply: '',
-      totalSupply: '',
-      supplyType: '',
-      accessType: '',
-      transferType: '',
-      tokenType: '',
-      network: '',
+      tokenName: "",
+      tokenSymbol: "",
+      tokenDecimals: "",
+      initialSupply: "",
+      totalSupply: "",
+      supplyType: "",
+      accessType: "",
+      transferType: "",
+      tokenType: "",
+      network: "",
     },
     validationSchema: LoginSchema,
     onSubmit: (values) => {
       // alert("you clicked")
       // alert(JSON.stringify(values, null, 2))
-      console.log(values)
-      deployFunction(values)
+      console.log(values);
+      deployFunction(values);
     },
-  })
+  });
 
   // here we get the value of an input
   function handleChange(event) {
     console.log(event.target.value);
-    if(event.target.value==1){
+    if (event.target.value == 1) {
       setValue(true);
-    }
-    else{
+    } else {
       setValue(false);
     }
-
   }
 
   // ..............................................
   const deployFunction = async (value) => {
     // get signer
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    await provider.send('eth_requestAccounts', [])
-    const signer = await provider.getSigner()
-    const address = await signer.getAddress()
-    console.log(address, 'this is signer aqddress')
-    const factory = new ContractFactory(abi, byteCode, signer)
-    console.log(value.tokenName, 'this is in deploy function')
-    console.log(value.tokenSymbol, 'this is total supply')
-    setSymbol(value.tokenSymbol)
-    setName(value.tokenName)
-    setDecimals(value.tokenDecimals)
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = await provider.getSigner();
+    const address = await signer.getAddress();
+    console.log(address, "this is signer aqddress");
+    const factory = new ContractFactory(abi, byteCode, signer);
+    console.log(value.tokenName, "this is in deploy function");
+    console.log(value.tokenSymbol, "this is total supply");
+    setSymbol(value.tokenSymbol);
+    setName(value.tokenName);
+    setDecimals(value.tokenDecimals);
     const contract = await factory.deploy(
       value.tokenName,
       value.tokenSymbol,
       value.tokenDecimals,
       value.initialSupply,
-      value.totalSupply,
-    )
-    console.log(contract.address)
-    setAddress(contract.address)
-    console.log(contract.deployTransaction)
-    
-  }
+      value.totalSupply
+    );
+    console.log(contract.address);
+    setAddress(contract.address);
+    console.log(contract.deployTransaction);
+  };
   // ...............................................
   const connectWallet = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    await provider.send('eth_requestAccounts', [])
-    const signer = provider.getSigner()
-    const address = await signer.getAddress()
-    console.log(address)
-    setConnect(true)
-  }
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const address = await signer.getAddress();
+    console.log(address);
+    setConnect(true);
+  };
 
   // auto import token funciton in metamask wallet
   const autoImportToken = async () => {
-    const tokenAddress = address
-    const tokenSymbol = symbol
-    const tokenDecimals = decimals
+    const tokenAddress = address;
+    const tokenSymbol = symbol;
+    const tokenDecimals = decimals;
     try {
       // wasAdded is a boolean. Like any RPC method, an error may be thrown.
       const wasAdded = await window.ethereum.request({
-        method: 'wallet_watchAsset',
+        method: "wallet_watchAsset",
         params: {
-          type: 'ERC20', // Initially only supports ERC20, but eventually more!
+          type: "ERC20", // Initially only supports ERC20, but eventually more!
           options: {
             address: tokenAddress, // The address that the token is at.
             symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
             decimals: tokenDecimals, // The number of decimals in the token
           },
         },
-      })
+      });
 
       if (wasAdded) {
-        console.log('Thanks for your interest!')
+        console.log("Thanks for your interest!");
       } else {
-        console.log('Your loss!')
+        console.log("Your loss!");
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
   // ...............................................
 
   return (
@@ -121,9 +118,9 @@ const First = () => {
         <div className="container" id="main">
           <div className="container" id="upper">
             <div id="up1" className="col-lg-8 col-md-7 col-sm-8 ">
-              {' '}
-              <h4 style={{ fontSize: '24px', fontWeight: '500' }}>
-                Token Details
+              {" "}
+              <h4 style={{ fontSize: "24px", fontWeight: "500" }}>
+                Token Details With Vercel And Github Actions
               </h4>
               <p id="small">Enter token details and choose your network</p>
               <h4>Token Name :{name}</h4>
@@ -131,7 +128,7 @@ const First = () => {
             </div>
             <div id="connectBtn" className="col-lg-4 col-md-4 col-sm-2 col-6">
               <button className="cntBtn" onClick={connectWallet}>
-                {connect ? 'Connected' : 'Connect Wallet'}
+                {connect ? "Connected" : "Connect Wallet"}
               </button>
             </div>
           </div>
@@ -150,19 +147,19 @@ const First = () => {
                     class="form-control"
                     name="tokenName"
                     onChange={formik.handleChange}
-                    style={{ display: 'block' }}
+                    style={{ display: "block" }}
                     placeholder="Your Token Name"
                     value={formik.values.tokenName}
                     onBlur={formik.handleBlur}
                   />
                 </div>
                 {formik.errors.tokenName && (
-                  <div className="error" style={{ color: 'red' }}>
+                  <div className="error" style={{ color: "red" }}>
                     {formik.errors.tokenName}
                   </div>
                 )}
                 <p id="text">
-                  choose a name for your token{' '}
+                  choose a name for your token{" "}
                   {/* <button type="submit">enter</button> */}
                 </p>
               </div>
@@ -176,13 +173,13 @@ const First = () => {
                     class="form-control"
                     name="tokenSymbol"
                     onChange={formik.handleChange}
-                    style={{ display: 'block' }}
+                    style={{ display: "block" }}
                     placeholder="Your Token Symbol"
                     value={formik.values.tokenSymbol}
                     onBlur={formik.handleBlur}
                   />
                 </div>
-                <div className="error" style={{ color: 'red' }}>
+                <div className="error" style={{ color: "red" }}>
                   {formik.errors.tokenSymbol}
                 </div>
 
@@ -201,7 +198,7 @@ const First = () => {
                     name="tokenDecimals"
                     value={formik.values.tokenDecimals}
                     onChange={formik.handleChange}
-                    style={{ display: 'block' }}
+                    style={{ display: "block" }}
                     placeholder="18"
                     onBlur={formik.handleBlur}
 
@@ -209,7 +206,7 @@ const First = () => {
                   />
                 </div>
                 {formik.errors.tokenDecimals && (
-                  <div className="error" style={{ color: 'red' }}>
+                  <div className="error" style={{ color: "red" }}>
                     {formik.errors.tokenDecimals}
                   </div>
                 )}
@@ -229,11 +226,11 @@ const First = () => {
                     name="initialSupply"
                     onChange={formik.handleChange}
                     onBlur={formik.handleBlur}
-                    style={{ display: 'block' }}
+                    style={{ display: "block" }}
                     placeholder="Your Token Initial Supply"
                   />
                 </div>
-                <div className="error" style={{ color: 'red' }}>
+                <div className="error" style={{ color: "red" }}>
                   {formik.errors.initialSupply}
                 </div>
                 <p id="text">
@@ -252,11 +249,11 @@ const First = () => {
                     name="totalSupply"
                     onChange={formik.handleChange}
                     onBlur={formik.handleBlur}
-                    style={{ display: 'block' }}
+                    style={{ display: "block" }}
                     placeholder="Your Token Max Supply"
                   />
                 </div>
-                <div className="error" style={{ color: 'red' }}>
+                <div className="error" style={{ color: "red" }}>
                   {formik.errors.totalSupply}
                 </div>
                 <p id="text">Insert the maximum number of tokens available.</p>
@@ -276,7 +273,7 @@ const First = () => {
                         class="form-control"
                         name="supplyType"
                         onChange={formik.handleChange}
-                        style={{ display: 'block' }}
+                        style={{ display: "block" }}
                         placeholder="Enter Token Name"
                         value="Fixed"
                       />
@@ -305,7 +302,7 @@ const First = () => {
                         class="form-control"
                         name="supplyType"
                         onChange={formik.handleChange}
-                        style={{ display: 'block' }}
+                        style={{ display: "block" }}
                         placeholder="Enter Token Name"
                         value="None"
                       />
@@ -333,7 +330,7 @@ const First = () => {
                       class="form-control"
                       name="transferType"
                       onChange={formik.handleChange}
-                      style={{ display: 'block' }}
+                      style={{ display: "block" }}
                       placeholder="Enter Token Name"
                       value="Unstopable"
                     />
@@ -365,14 +362,14 @@ const First = () => {
                 </div>
                 <div id="t2" className="col-12">
                   <p id="text">
-                    {' '}
+                    {" "}
                     Your Token Source Code will be automatically verified on
                     Etherscan. Only works on Mainnet.
                   </p>
                 </div>
                 <div id="t3" className="col-12">
                   <div id="btn">
-                    {' '}
+                    {" "}
                     <Form>
                       <Form.Check type="switch" id="custom-switch" />
                     </Form>
@@ -382,10 +379,10 @@ const First = () => {
                   </div>
                 </div>
                 <div className="row">
-                  {' '}
+                  {" "}
                   <div id="t4" className="col-lg-12 col-md-12 col-sm-12 col-12">
                     <p id="text">
-                      {' '}
+                      {" "}
                       Remove the link pointing from your contract.
                     </p>
                   </div>
@@ -393,9 +390,9 @@ const First = () => {
 
                 <div id="t5">
                   <div id="inner51">
-                    {' '}
+                    {" "}
                     <div id="btn">
-                      {' '}
+                      {" "}
                       <Form>
                         <Form.Check type="switch" id="custom-switch" />
                       </Form>
@@ -405,9 +402,9 @@ const First = () => {
                     </div>
                   </div>
                   <div id="inner52">
-                    {' '}
+                    {" "}
                     <div id="btn">
-                      {' '}
+                      {" "}
                       <Form>
                         <Form.Check type="switch" id="custom-switch" />
                       </Form>
@@ -417,9 +414,9 @@ const First = () => {
                     </div>
                   </div>
                   <div id="inner53">
-                    {' '}
+                    {" "}
                     <div id="btn">
-                      {' '}
+                      {" "}
                       <Form>
                         <Form.Check type="switch" id="custom-switch" />
                       </Form>
@@ -429,9 +426,9 @@ const First = () => {
                     </div>
                   </div>
                   <div id="inner54">
-                    {' '}
+                    {" "}
                     <div id="btn">
-                      {' '}
+                      {" "}
                       <Form>
                         <Form.Check type="switch" id="custom-switch" />
                       </Form>
@@ -472,11 +469,10 @@ const First = () => {
                 <div id="a2">
                   <select
                     id="input"
-                    onChange={handleChange} 
+                    onChange={handleChange}
                     name="network"
                     class="form-select"
-                    aria-label="Default select example" 
-                    
+                    aria-label="Default select example"
                   >
                     <option value="1">Rinkyby-Testnet</option>
                     <option selected>Etherium-Mainnet</option>
@@ -486,10 +482,11 @@ const First = () => {
                   </select>
                 </div>
                 <p id="text">Choose your Network.</p>
-                {value ? <div id="selectNetwork">
-                  <>You selected a TEST Network</>
-                </div> :  null}
-               
+                {value ? (
+                  <div id="selectNetwork">
+                    <>You selected a TEST Network</>
+                  </div>
+                ) : null}
               </div>
 
               {/* last */}
@@ -527,7 +524,7 @@ const First = () => {
                   onClick={autoImportToken}
                   className="cntBtn2 col-lg-12 col-md-12 col-sm-12 col-12"
                 >
-                  Auto Import Token{' '}
+                  Auto Import Token{" "}
                 </button>
               </div>
             </div>
@@ -537,7 +534,7 @@ const First = () => {
         {/* second */}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default First
+export default First;
